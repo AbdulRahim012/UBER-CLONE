52ed625dbcdfb12f422183b3b34ed94511d119fd
Abdul_Rahim <ar818.abdulrahim@gmail.com>
2024-12-27 13:53:25 +0530

    captain registeration done

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,15 @@ const express = require("express");
 const cors = require("cors");
 // Initialize Express App
 const app = express();
+const cookieParser=require('cookie-parser');
 const connectToDb=require('./db/db');
 const userRoutes= require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 connectToDb();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 
 // Routes
 app.get("/", (req, res) => {
@@ -18,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/users',userRoutes);
+app.use('/captains', captainRoutes);
 
 // Export the app
 module.exports = app;
